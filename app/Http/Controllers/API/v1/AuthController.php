4766d2d79e83a97e3528abd91ea06e69931f00bf
Noval Dwi Putra <nvldwiptr@gmail.com>
2020-09-22 20:34:29 +0700

    Upload Mentahan dan Setting Passport

---
+++ app/Http/Controllers/API/v1/AuthController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\API\v1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Model\CEO;
+
+class AuthController extends Controller
+{
+
+}
